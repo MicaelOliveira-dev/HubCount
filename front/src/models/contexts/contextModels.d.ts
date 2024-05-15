404bb1159e93ad0ba8a3eb97801574ff47c71666
MicaelOliveira-dev <micaeloliveiradev@hotamil.com>
2024-05-15 09:57:29 -0300

    x

--- front/src/models/contexts/contextModels.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-
-export interface ChildrenPropsModel {
-    children: React.ReactNode
-}
--- front/src/models/entities/NavLink.d.ts
+++
@@ -1,7 +0,0 @@
-import { IconType } from 'react-icons'
-export interface NavbarLink {
-    linkTo: string;
-    linkText?: string;
-    linkIcon?: IconType;
-    show?: boolean;
-}
